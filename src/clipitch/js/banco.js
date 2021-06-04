26279ba520ad0848982c20f1ad01a901dc8e7cfc
Halex Vieira <33609708+halexmaciel@users.noreply.github.com>
2021-06-04 17:14:35 -0400

    Upload dos arquivos JS para desenvolvimento

--- src/clipitch/js/banco.js
+++ src/clipitch/js/banco.js
@@ -2,35 +2,42 @@
 
 let db = "";
 
+// Cria Banco de Dados no Browser do Usuário
 const criaBancoDeDados = (TopClips) => {
   // Verifica se o Browser é compatível com IndexedDb
   if (window.indexedDB) {
-    const request = window.indexedDB.open("topClipsDB", 1);
+    const bancoRequest = window.indexedDB.open("topClipsDB", 1);
 
-    request.onupgradeneeded = (e) => {
+    bancoRequest.onupgradeneeded = (e) => {
+      console.log("Passou pelo Upgrade");
+
       // Obtém o banco de dados
       db = e.target.result;
 
-      // ObjectStorage = Tabela
-      const objectStorageClips = db.createObjectStore("clips", {
-        keyPath: "broadcast_id"
-      });
+      if (!db.objectStoreNames.contains("clips")) {
+        // ObjectStorage = Tabela
+        const clipsTb = db.createObjectStore("clips", {
+          autoIncrement: true,
+        });
 
-      objectStorageClips.createIndex("title", "titleIdx", { unique: false });
+        clipsTb.createIndex("title", "titleIdx", {
+          unique: false,
+          multiEntry: true,
+        });
+      }
     };
 
-    request.onsuccess = (e) => {
+    bancoRequest.onsuccess = (e) => {
+      console.log("Passou pelo Sucesso");
+
       db = e.target.result;
 
-      for (let i in TopClips) {
-        console.log(TopClips[i]);
-        addClip(db, TopClips[i]);
-      }
+      adicionarClipsBD(db, TopClips);
 
       console.log("Sucesso ao criar o banco de dados");
     };
 
-    request.onerror = (e) => {
+    bancoRequest.onerror = (e) => {
       console.log("Erro ao criar o Banco de Dados", e);
     };
   } else {
@@ -39,19 +46,134 @@ const criaBancoDeDados = (TopClips) => {
 };
 
 // Adiciona os Clips no IndexedDb
-const addClip = (db, clip) => {
+const adicionarClipsBD = (db, clips) => {
   const transactionAdd = db.transaction("clips", "readwrite");
   const objectStorageClip = transactionAdd.objectStore("clips");
 
-  const request = objectStorageClip.put(clip);
+  console.log(`Quantidade de Registros em Clips ${clips.length}`);
+
+  // Adiciona/Atualiza os vídeos obtidos da API do Twitch um por um no Banco de dados
+
+  clips.forEach((clip) => {
+    objectStorageClip.put(clip);
+  });
 
   transactionAdd.oncomplete = (e) => {
-    console.log("Transação completada");
+    console.log("Transação concluída");
   };
 
   transactionAdd.onerror = (e) => {
     console.log("Erro ao realizar a inclusão de registro no banco de dados");
   };
 };
+
+function getAllClips(db) {
+  var tabela = document.getElementById("videos");
+  tabela.innerHTML = "";
+
+  var s = "";
+
+  var transaction = db.transaction("clips", "readonly");
+  var clips = transaction.objectStore("clips");
+  var cursor = clips.openCursor();
+
+  cursor.onsuccess = function (e) {
+    var cursor = e.target.result;
+    if (cursor) {
+      for (var field in cursor.value) {
+        s += field + "=" + cursor.value.title + "<br/>";
+      }
+      s += "</p>";
+      cursor.continue();
+    }
+  };
+
+  transaction.oncomplete = () => {
+    tabela.innerHTML = s;
+  };
+}
+
+
+function searchClips(db, searchText) {
+  var tabela = document.getElementById("videos");
+  tabela.innerHTML = "";
+
+  var s = "";
+
+  var transaction = db.transaction("clips", "readonly");
+  var clips = transaction.objectStore("clips");
+  var cursor = clips.openCursor();
+
+  cursor.onsuccess = function (e) {
+    var cursor = e.target.result;
+    if (cursor) {
+      for (var field in cursor.value) {
+        s += field + "=" + cursor.value.title + "<br/>";
+      }
+      s += "</p>";
+      cursor.continue();
+    }
+  };
+
+  transaction.oncomplete = () => {
+    tabela.innerHTML = s;
+  };
+}
+
+
+function listClipsDay(db) {
+  var tabela = document.getElementById("videos");
+  tabela.innerHTML = "";
+
+  var s = "";
+
+  var transaction = db.transaction("clips", "readonly");
+  var clips = transaction.objectStore("clips");
+  var cursor = clips.openCursor();
+
+  cursor.onsuccess = function (e) {
+    var cursor = e.target.result;
+    if (cursor) {
+      for (var field in cursor.value) {
+        s += field + "=" + cursor.value.title + "<br/>";
+      }
+      s += "</p>";
+      cursor.continue();
+    }
+  };
+
+  transaction.oncomplete = () => {
+    tabela.innerHTML = s;
+  };
+}
+
+
+function listClipsWeek(db) {
+  var tabela = document.getElementById("videos");
+  tabela.innerHTML = "";
+
+  var s = "";
+
+  var transaction = db.transaction("clips", "readonly");
+  var clips = transaction.objectStore("clips");
+  var cursor = clips.openCursor();
+
+  cursor.onsuccess = function (e) {
+    var cursor = e.target.result;
+    if (cursor) {
+      for (var field in cursor.value) {
+        s += field + "=" + cursor.value.title + "<br/>";
+      }
+      s += "</p>";
+      cursor.continue();
+    }
+  };
+
+  transaction.oncomplete = () => {
+    tabela.innerHTML = s;
+  };
+}
+
+
 
 export default criaBancoDeDados;
--- src/clipitch/js/script.js
+++ src/clipitch/js/script.js
@@ -1,13 +1,13 @@
-import criaBancoDeDados from './banco.js';
+import criaBancoDeDados from "./banco.js";
 
 // Constantes, Parâmetros e Funções Necessárias para Requisição da API do Twitch e Armazenamento dos Dados
-
 const URL_AUTH = "https://id.twitch.tv/oauth2/token";
 const URL_CLIPS = "https://api.twitch.tv/kraken/clips/top";
 const LIMIT = 100;
 const CLIENT_ID = "j1c6sfsv12hl349hh871nneegu8frw";
-const CLIENT_SECRET = "46wet0fcatmo14q5ccj1cbpufbcr7d";
+const CLIENT_SECRET = "di18gij8eeha9e51fak5ytgsnf65qw";
 
+// Cabeçalho necessário para autenticação e obtenção do Token
 const PARAMS_AUTH = {
   method: "POST",
   headers: {
@@ -22,18 +22,18 @@ const PARAMS_AUTH = {
   }),
 };
 
+// Cabeçalho necessário para obtenção dos clips
 const HEADERS_CLIPS = {
   Accept: "application/vnd.twitchtv.v5+json",
   "Client-ID": CLIENT_ID,
 };
 
+// Parâmetros opcionais para customização na busca dos dados da API do Twitch
 const PARAMS_CLIPS = {
-  channel: null,
-  cursor: null,
-  game: null,
-  language: null,
+  first: 0,
+  after: null,
   limit: LIMIT,
-  period: "day",
+  period: "week",
   trending: true,
 };
 
@@ -43,7 +43,9 @@ const conectaTwitch = () => {
     .then((res) => res.json())
     .then((res) => {
       const access_token = res.access_token;
-      getTwitchDados(access_token, PARAMS_CLIPS);
+
+      //Executa a função que obtém os clips do Twitch
+      getTwitchDados(access_token, URL_CLIPS, PARAMS_CLIPS);
     })
     .catch((err) => {
       console.log("Erro ao autenticar!!!", err);
@@ -51,28 +53,31 @@ const conectaTwitch = () => {
 };
 
 // Realiza a chamada da API Top Clips do Twitch
-const getTwitchDados = (auth_token, params) => {
+const getTwitchDados = (token, url, params) => {
+  
   const clipsData = [];
-  fetch(URL_CLIPS, {
+
+  url += "?" + new URLSearchParams(params).toString();
+  
+  console.log(url)
+  
+  fetch(url, {
     method: "GET",
     headers: HEADERS_CLIPS,
   })
-    .then((res) => res.json())
-    .then((topClips) => {
+  .then((res) => res.json())
+  .then((topClips) => {
+    console.log(topClips)
       clipsData.push(...topClips.clips);
-      
       criaBancoDeDados(clipsData);
-      /**clipsData.map((clip) => {
-        localStorage.setItem("broadcast_id", clip.broadcast_id);
-        localStorage.setItem("created_at", clip.created_at);
-      });*/
-
     }).catch((err) => {
       console.log("Erro ao obter clips", err);
     });
+    
 };
 
-document.addEventListener("DOMContentLoaded",conectaTwitch);
+document.addEventListener("DOMContentLoaded", conectaTwitch);
 
 export default { conectaTwitch, getTwitchDados };
+
 

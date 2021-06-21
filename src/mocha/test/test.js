b646cc3030b7d359bfa106a54bb79e64e86228c3
Gustavo Castro <gstvcastroc@gmail.com>
2021-06-20 22:04:54 -0300

    Implementação Mocha JS testes. #45

---
+++ src/mocha/test/test.js
@@ -0,0 +1,38 @@
+import criaBancoDeDados from "/src/clipitch/js/banco.js";
+
+const assert = chai.assert;
+const expect = chai.expect;
+const should = chai.should();
+
+describe("Testes de unidade para o ClipItch", () => {
+    describe("#conectaTwitch()", () => {
+        it('Cria o banco de dados e exerce as funções atribuídas dentro da mesma chamada', () => {
+         //expect(criaBancoDeDados()).to.be.any;
+         //const retorno = chai.spy.returns(criaBancoDeDados());
+         //var spy = criaBancoDeDados.spy.returns(true);
+         //on(criaBancoDeDados(), spy)
+         //expect(criaBancoDeDados()).to.have.been.called();
+         expect(criaBancoDeDados()).to.have.been.all;
+         //spy.On(criaBancoDeDados());
+        })       
+    });
+    describe('#Teste()', function() {
+        context('when not present', function() {
+          it('should not throw an error', function() {
+            (function() {
+              [1, 2, 3].indexOf(4);
+            }.should.not.throw());
+          });
+          it('should return -1', function() {
+            [1, 2, 3].indexOf(4).should.equal(-1);
+          });
+        });
+        context('when present', function() {
+          it('should return the index where the element first appears in the array', function() {
+            [1, 2, 3].indexOf(3).should.equal(2);
+          });
+        });
+      });
+});
+
+mocha.run();
